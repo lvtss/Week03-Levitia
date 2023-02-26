4aa76b1fd58ce348ec16ac34015c28a897a62edb
lvtss <112721368+lvtss@users.noreply.github.com>
2023-02-26 12:19:59 -0500

    innit

--- Week03-Levitia/Week03-Levitia/Data.swift
+++ Week03-Levitia/Week03-Levitia/Data.swift
@@ -33,24 +33,7 @@ let information = Info(
     name: "  David Hockney  ",
     story: "David Hockney, one of the most influential and British artists of the 21 century and a key figure of the pop art movement, who was one of the first to openly speak in the language of painting and photography, turned 82 this year.\n\nHockney has always returned to painting portraits throughout his career. He painted life-size portraits and double portraits of friends, lovers and relatives in a realistic style that artfully conveyed the similarity of his objects.\n\nOne important theme in the artist's work were portraits made in the technique of naturalism. Preparation for such works took a lot of time and effort: Hockney drew numerous sketches, sketches and used photographs for greater accuracy, he could redraw faces and details of paintings ten times. \n\nBy depicting almost life-size figures, David wanted to achieve the effect of an almost real presence of the characters next to the audience. Perhaps more, Hockney's portraits turned to his own figure year after year, creating more than 300 self-portraits.",
     
-//    hobbies: ["bicycle", "ticket.fill", "book.fill"],
-//    foods: ["🥐", "🌮", "🍣"],
-
-//
-//    colors: [Color.blue, Color.purple, Color.pink],
-//    randomPainting: [
-//        "Hockney",
-//        "3500",
-    
-    
     gallery: ["Hockney-01","Hockney-02", "Hockney-03", "Hockney-04", "Hockney-05", "Hockney-06", "Hockney-07"]
 )
-//        "Prince’s last name was Nelson.",
-//        "503 new species were discovered in 2020.",
-//        "Ice is 9 percent less dense than liquid water.",
-//        "You can spell every number up to 1,000 without using the letter A.\n\n...one, two, three, four...ninety-nine...nine hundred ninety-nine 🧐",
-//        "A collection of hippos is called a bloat.",
-//        "White sand beaches are made of parrotfish poop.",
-//    ]
-//)
+
 
